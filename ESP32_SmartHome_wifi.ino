92e8302e316bfa2252e07860275f49eee3b3f529
kayedhom <braaadel78@gmail.com>
2024-04-23 17:54:46 +0200

    Init Source Code and sample HTML page

---
+++ ESP32_SmartHome_wifi.ino
@@ -0,0 +1,388 @@
+
+#include <Robojax_L298N_DC_motor.h>
+// motor 1 settings
+#define CHA 0
+#define ENA 19  // this pin must be PWM enabled pin if Arduino board is used
+#define IN1 18
+#define IN2 5
+
+// motor 2 settings
+#define IN3 17
+#define IN4 16
+#define ENB 4  // this pin must be PWM enabled pin if Arduino board is used
+#define CHB 1
+
+const int CCW = 2;  // do not change
+const int CW = 1;   // do not change
+
+#define motor1 1  // do not change
+#define Led 2  // do not change
+
+// for single motor
+//Robojax_L298N_DC_motor motor(IN1, IN2, ENA, CHA, true);
+
+// for two motors without debug information // Watch video instruciton for this line: https://youtu.be/2JTMqURJTwg
+Robojax_L298N_DC_motor motor(IN1, IN2, ENA, CHA, IN3, IN4, ENB, CHB);
+
+// fore two motors with debut information
+//Robojax_L298N_DC_motor motor(IN1, IN2, ENA, CHA, IN3, IN4, ENB, CHB, true);
+
+
+int motor1Direction = CW;         //default direction of rotation
+const int motor1changeStep = 10;  // 10 is 10% every time button is pushed
+int motor1Speed = 40;             // variable holding the light output vlaue (initial value) 40 means 40%
+const int motor1MinimumSpeed = 20;
+const int motor1MaximumSpeed = 100;
+int motor1StopState = HIGH;  //Stope state of motor (HIGH means STOP) and LOW means Start
+
+
+int Switch_one = HIGH;  //Stope state of Switch (HIGH means STOP) and LOW means Start
+int Switch_two = HIGH;  //Stope state of Switch (HIGH means STOP) and LOW means Start
+
+
+
+#include "ESP32_L298N_DC_motor_wifi_page.h"
+
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+
+const char *ssid = "POCO X3 NFC";
+const char *password = "abcd1234";
+bool passwordSubmitted = false;
+String submittedPassword; // Variable to store the submitted password
+
+WebServer server(80);
+
+const int led = 13;
+
+
+void handleFirstPage() {
+String firstPage = "<!DOCTYPE html><html><head><meta charset=\"UTF-8\"><title>First Page</title>";
+firstPage += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
+firstPage += "<title>First Page</title>";
+  firstPage += "<style>";
+  firstPage += "body {";
+  firstPage += "background-image: linear-gradient(to right, #ffcccc, #e6ccff); /* Gradient background */";
+  firstPage += "background-attachment: fixed; /* Keep background fixed while scrolling */";
+  firstPage += "font-family: 'Arial', sans-serif; /* Change font family for better readability */";
+  firstPage += "color: #333; /* Set default text color */";
+  firstPage += "margin: 0; /* Remove default margin for full-width background */";
+  firstPage += "padding: 0; /* Remove default padding */";
+  firstPage += "}";
+  firstPage += "form {";
+  firstPage += "background-color: #f4cccc;";
+  firstPage += "padding: 20px;";
+  firstPage += "border-radius: 8px;";
+  firstPage += "box-shadow: 0 2px 4px rgba(0,0,0,0.1);";
+  firstPage += "max-width: 300px;";
+  firstPage += "width: 100%;";
+  firstPage += "}";
+  firstPage += "input[type='password'],";
+  firstPage += "input[type='submit'] {";
+  firstPage += "width: 100%;";
+  firstPage += "padding: 10px;";
+  firstPage += "margin: 5px 0;";
+  firstPage += "box-sizing: border-box;";
+  firstPage += "border: 1px solid #ccc;";
+  firstPage += "border-radius: 5px;";
+  firstPage += "font-size: 16px;";
+  firstPage += "}";
+  firstPage += "input[type='submit'] {";
+  firstPage += "background-color: #e06666;";
+  firstPage += "cursor: pointer;";
+  firstPage += "}";
+  firstPage += "input[type='submit']:hover {";
+  firstPage += "background-color: #cc0000;";
+  firstPage += "}";
+  firstPage += "</style>";
+  firstPage += "</head><body>";
+  firstPage += "<h2>WELCOME HOME!</h2>";
+  firstPage += "<form action='/submit' method='POST'>";
+  firstPage += "<label for='password'>Password:</label><br>";
+  firstPage += "<input type='password' id='password' name='password'><br><br>";
+  firstPage += "<input type='submit' value='Submit'></form>";
+  firstPage += "</body></html>";
+
+
+
+  server.send(200, "text/html", firstPage);
+}
+
+
+
+void handleRoot() {
+    if (passwordSubmitted) {
+    // If password is submitted, serve the second page
+
+  String HTML_page = motorControlHeader_1;
+
+  HTML_page.concat(".bar1 {width: " + String(motor1Speed) + "%;}\n");
+  //HTML_page.concat(motor1Speed);
+  //HTML_page.concat("%;}");
+  //HTML_page.concat(".bar2 {width: " + "String(LedSpeed)" + "%;}\n");
+
+  HTML_page.concat(motorControlHeader_2);
+
+  HTML_page.concat(motor1Control_p1);
+
+  if (motor1Direction == CW) {
+    if (motor1StopState == HIGH) {
+      HTML_page.concat("<strong>Stopped - CW at ");
+    } else {
+      HTML_page.concat("<strong>Running - CW at ");
+    }
+  } else {
+    if (motor1StopState == HIGH) {
+      HTML_page.concat("<strong>Stopped - CCW at ");
+    } else {
+      HTML_page.concat("<strong>Running - CCW at ");
+    }
+  }
+  HTML_page.concat(motor1Speed);
+  HTML_page.concat(motor1Control_p2);
+  if (motor1StopState == HIGH) {
+    HTML_page.concat("m0START\">START");
+  } else {
+    HTML_page.concat("m0STOP\">STOP");
+  }
+  HTML_page.concat(motor1Control_p3);
+
+  ///Switch_one begins
+  HTML_page.concat(Switch_one_control_p1);
+
+    if (Switch_one == HIGH) {
+      HTML_page.concat("<strong>Switch off ");
+    } else {
+      HTML_page.concat("<strong>Switch on ");
+    }
+
+  
+  HTML_page.concat(Switch_one_control_p2);
+  if (Switch_one == HIGH) {
+    HTML_page.concat("m1START\">ON");
+  } else {
+    HTML_page.concat("m1STOP\">OFF");
+  }
+  HTML_page.concat(Switch_one_control);
+  ///Switch_one ends
+
+
+
+
+ ///Switch_two begins
+  HTML_page.concat(Switch_two_control_p1);
+
+    if (Switch_two == HIGH) {
+      HTML_page.concat("<strong>Switch off ");
+    } else {
+      HTML_page.concat("<strong>Switch on ");
+    }
+
+  
+  HTML_page.concat(Switch_two_control_p2);
+  if (Switch_two == HIGH) {
+    HTML_page.concat("m2START\">ON");
+  } else {
+    HTML_page.concat("m2STOP\">OFF");
+  }
+  HTML_page.concat(Switch_two_control);
+  ///Switch_two Ends
+
+
+  HTML_page.concat("</body>\n</html>");
+
+  server.send(200, "text/html", HTML_page);
+
+    } else {
+    // Otherwise, serve the first page
+    handleFirstPage();
+  }
+}
+
+void handleNotFound() {
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+void setup(void) {
+  pinMode (2, OUTPUT);
+  Serial.begin(115200);
+  motor.begin();
+  //L298N DC Motor 
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+
+  if (MDNS.begin("ESP32")) {
+    Serial.print("MDNS responder started at http://");
+    Serial.println("ESP32 Smart Home");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/submit", HTTP_POST, [](){
+    // Handle form submission
+    submittedPassword = server.arg("password"); // Retrieve and store the submitted password
+
+    // Redirect to the root URL (refresh page)
+    server.sendHeader("Location", "/");
+    server.send(302, "text/plain", "");
+  });
+
+
+  server.on("/speed", HTTP_GET, handleMotorSpeed);
+  server.on("/direction", HTTP_GET, handleMotorDirection);
+  server.on("/stop", HTTP_GET, handleMotorBrake);
+  server.onNotFound(handleNotFound);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+      if(submittedPassword == "good"){passwordSubmitted = true;}
+  server.handleClient();
+
+
+  if (motor1StopState == HIGH) {
+    motor.brake(motor1);
+
+  } else {
+    motor.rotate(motor1, motor1Speed, motor1Direction);  //run motor1 at motor1Speed% speed in motor1Direction
+  }
+
+
+
+  delay(100);
+}
+
+
+/*
+ * handleMotorSpeed()
+ * Slows down or speeds up the motor
+ * returns nothing
+ */
+void handleMotorSpeed() {
+  if (server.arg("do") == "m1slower") {
+    motor1Speed -= motor1changeStep;
+
+    if (motor1Speed < motor1MinimumSpeed) {
+      motor1Speed = motor1MinimumSpeed;
+    }
+  } else if (server.arg("do") == "m1faster") {
+    motor1Speed += motor1changeStep;
+
+    if (motor1Speed > motor1MaximumSpeed) {
+      motor1Speed = motor1MaximumSpeed;
+    }
+  } else {
+    motor1Speed = 0;
+  }
+
+  handleRoot();
+} 
+
+/*
+ * handleMotorDirection()
+ * changes the direction of motor
+ * returns nothing
+ */
+void handleMotorDirection() {
+  if (server.arg("dir") == "m1CW") {
+    motor1Direction = CW;
+
+  } else if (server.arg("dir") == "m1CCW") {
+    motor1Direction = CCW;
+
+  } else {
+  }
+
+  handleRoot();
+}  
+
+/*
+ * handleMotorBrake()
+ * applies brake to the motor
+ * returns nothing
+ */
+void handleMotorBrake() {
+  if (server.arg("do") == "m0START") {
+    motor1StopState = LOW;
+  } else if (server.arg("do") == "m0STOP") {
+    motor1StopState = HIGH;
+  } else if (server.arg("do") == "m1START") {
+    Switch_one = LOW;
+    Switch_one_ON_func();
+
+  }
+  else if (server.arg("do") == "m1STOP") {
+    Switch_one = HIGH;
+    Switch_one_OFF_func();
+
+  }
+  else if (server.arg("do") == "m2START") {
+    Switch_two = LOW;
+    Switch_two_ON_func();
+
+  }
+  else if (server.arg("do") == "m2STOP") {
+    Switch_two = HIGH;
+    Switch_two_OFF_func();
+
+  }
+  
+   else {
+
+  }
+  handleRoot();
+}  
+void Switch_one_ON_func(){
+       digitalWrite(2, HIGH);
+}
+
+void Switch_one_OFF_func(){
+        digitalWrite(2, LOW);
+
+}
+
+
+void Switch_two_ON_func(){
+       digitalWrite(2, HIGH);
+}
+
+void Switch_two_OFF_func(){
+        digitalWrite(2, LOW);
+
+}
---
+++ ESP32_SmartHome_wifi_page.h
@@ -0,0 +1,360 @@
+const char motorControlHeader_1[]  PROGMEM = R"header1(
+<!DOCTYPE html>
+<html>
+<head>
+<title> ESP32 Motor Control</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {box-sizing: border-box}
+.table{
+    width:100%;
+    display:table;
+}
+.table-Control{
+    width:100%;
+    display:table;
+    margin:20px
+}
+
+.row{
+    display:table-row;
+}
+
+.fixedCell {
+    width:15%;
+    display:table-cell;
+
+}
+
+/* Style for generic cells */
+.cell {
+    display: table-cell;
+    background: #72c3dc; /* Light blue background color */
+    padding: 10px; /* Add some padding for spacing */
+    border-radius: 5px; /* Add rounded corners */
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Add a subtle shadow for depth */
+}
+
+.container {
+    display: inline-block;
+    justify-content: space-between; /* Distribute space evenly between children */
+    align-items: center; /* Center items vertically */
+    padding: 10px 100px; /* Add padding for spacing */
+
+}
+
+.progress_bar {
+  font-size: 14px;  
+  text-align: right;
+  padding: 8px;
+  color: white;
+  background-color: #f0f0f0; /* Fallback color */
+  background-image: linear-gradient(to right, #d0e0e3, #a2c4c9 , #76a5af ,#3d85c6); /* Gradient background */
+  float:left;
+  border-radius: 5px; /* Add rounded corners */
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Add a subtle shadow for depth */
+  height: 50px;
+}
+
+
+.Status_bar {
+    text-align: right;
+    padding: 10px 20px;
+    color: white;
+    float: left;
+    background-image: linear-gradient(to right, #fff2cc, #e69138, #f9cb9c); /* Gradient background */
+    border-radius: 5px; /* Add rounded corners */
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Add a subtle shadow for depth */
+    font-size: calc(0.8em + 0.8vw); /* Dynamic font size based on width and viewport width */
+    white-space: nowrap; /* Prevent text from wrapping */
+
+}
+
+
+/* Style for button container */
+.buttonsDiv {
+    display: flex;
+    justify-content: center; /* Center align buttons horizontally */
+    align-items: center; /* Center align buttons vertically */
+    margin-top: 20px; /* Add some spacing above the buttons */
+}
+
+/* Style for buttons */
+.buttonsDiv button {
+    background-color: #4CAF50; /* Green background color */
+    color: white;
+    border: none;
+    border-radius: 5px; /* Add rounded corners */
+    padding: 10px 20px; /* Add padding for spacing */
+    margin: 0 10px; /* Add some spacing between buttons */
+    cursor: pointer; /* Change cursor to pointer on hover */
+    transition: background-color 0.3s; /* Add smooth transition for background color change */
+}
+
+/* Hover effect for buttons */
+.buttonsDiv button:hover {
+    background-color: #45a049; /* Darken background color on hover */
+}
+
+.startStop {
+    font-size: 20px;
+    background-color: #f44336;
+    color: #ffffff;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    margin: 0 10px; /* Add margin to create space between buttons */
+    cursor: pointer;
+    transition: background-color 0.3s;
+    text-decoration: none;
+    display: inline-flex; /* Use flexbox */
+    align-items: center; /* Align items vertically */
+}
+
+.startStop:hover {
+    background-color: #e74c3c; /* Darken background color on hover */
+    border-color: #e74c3c; /* Darken border color on hover */
+}
+
+.ONoff {
+    font-size: 20px;
+    background-color: #f44336;
+    color: #ffffff;
+    border: none;
+    border-radius: 5px;
+    padding: 0px 20px;
+    margin: 0 10px; /* Add margin to create space between buttons */
+    cursor: pointer;
+    transition: background-color 0.3s;
+    text-decoration: none;
+    display: inline-flex; /* Use flexbox */
+    align-items: center; /* Align items vertically */
+}
+
+.ONoff:hover {
+    background-color: #e74c3c; /* Darken background color on hover */
+    border-color: #e74c3c; /* Darken border color on hover */
+}
+
+)header1";
+
+
+
+const char motorControlHeader_2[] PROGMEM = R"P2( 
+
+
+/* Style for navigation links */
+.nextprev a.rj-right,
+.nextprev a.rj-left {
+    background-color: #FF7043; /* Reddish-orange background color */
+    color: #ffffff; /* White text color */
+    border-color: #FF7043; /* Reddish-orange border color */
+    padding: 10px 20px; /* Add padding for spacing */
+    border-radius: 5px; /* Add rounded corners */
+    text-decoration: none; /* Remove default underline */
+    transition: background-color 0.3s, color 0.3s, border-color 0.3s; /* Add smooth transition */
+}
+
+/* Hover effect for navigation links */
+.nextprev a.rj-right:hover,
+.nextprev a.rj-left:hover {
+    background-color: #E57373; /* Lighter red background color on hover */
+    border-color: #E57373; /* Lighter red border color on hover */
+}
+
+/* Style for buttons */
+.rj-btn,
+.rj-button {
+    border: none; /* Remove border */
+    display: inline-block; /* Display as inline-block */
+    padding: 5px 10px; /* Add padding for spacing */
+    border-radius: 5px; /* Add rounded corners */
+    text-decoration: none; /* Remove default underline */
+    color: #ffffff; /* White text color */
+    background-color: #FF7043; /* Reddish-orange background color */
+    cursor: pointer; /* Change cursor to pointer on hover */
+    transition: background-color 0.3s; /* Add smooth transition */
+    margin: 5px; /* Add margin for spacing */
+    text-align: center; /* Center align text */
+}
+
+/* Hover effect for buttons */
+.rj-btn:hover,
+.rj-button:hover {
+    background-color: #E57373; /* Lighter red background color on hover */
+}
+
+
+/* Add background color with gradient */
+body {
+    background-color: #f0f0f0; /* Fallback color */
+    background-image: linear-gradient(to right, #ffcccc, #e6ccff); /* Gradient background */
+    background-repeat: no-repeat;
+    background-attachment: fixed; /* Keep background fixed while scrolling */
+    font-family: 'Arial', sans-serif; /* Change font family for better readability */
+    color: #333; /* Set default text color */
+    margin: 0; /* Remove default margin for full-width background */
+    padding: 0; /* Remove default padding */
+
+}
+
+/* Optional: Add styling for specific sections or elements within the body */
+h1 {
+    font-size: 1.5em; /* Increase font size for headings */
+    margin-bottom: 20px; /* Add spacing below headings */
+    text-align: center; /* Center align headings */
+    text-transform: uppercase; /* Convert heading text to uppercase */
+}
+
+p {
+    font-size: 1.2em; /* Increase font size for paragraphs */
+    line-height: 1.6; /* Improve line spacing for better readability */
+    text-align: center; /* Center align paragraphs */
+}
+
+/* Additional CSS styles */
+
+</style>
+</head>
+<body>
+
+<h1>ESP32 Smart Home</h1>)P2";
+
+
+////////////////for motor 1 part 1///////////////
+const char motor1Control_p1[] PROGMEM = R"P3(
+<h2>Motor 1 Control</h2>
+<div class="table">
+    <div class="row">
+        <div class="fixedCell">
+            <div class="nextprev">
+            <a class="rj-left rj-btn" href="/speed?do=m1slower">Slower</a>
+            </div>        
+
+        </div><!--fixedCell -->
+
+
+        <div class="progress_bar bar1">)P3";
+
+
+////////////////for motor 1 part 2///////////////
+const char motor1Control_p2[] PROGMEM = R"P4(%</strong></div> 
+
+        <div class="fixedCell">
+        
+          <div class="nextprev">       
+          <a class="rj-right rj-btn" href="/speed?do=m1faster">Faster</a>
+          </div>       
+
+        </div><!--fixedCell -->
+    </div><!--row -->
+
+
+</div><!--table -->
+<hr align="center" width="50%">
+<div class="table">
+    <div class="row">
+        <div class="fixedCell">
+            <div class="nextprev">
+            <a class="rj-left rj-btn" href="/direction?dir=m1CCW">CCW</a>
+            </div>        
+
+        </div><!--fixedCell -->
+
+
+        <div class="buttonsDiv">
+            <a class="startStop" href="/stop?do=)P4";
+            
+ ////////////////for motor 1 part 3///////////////           
+const char motor1Control_p3[] PROGMEM = R"P5(            
+            </a>
+        </div> 
+
+        <div class="fixedCell">
+        
+          <div class="nextprev">       
+          <a class="rj-right rj-btn" href="/direction?dir=m1CW">CW</a>
+          </div>       
+
+        </div><!--fixedCell -->
+    </div><!--row - ESP.com -->
+
+
+</div><!-- ---- table motor 1 ended -->)P5";
+
+///////////////// motor 1 end******************************************
+///////////////////////*************************************
+
+////////////////for Led1///////////////
+
+
+////////////////for Led part 1///////////////
+const char Switch_one_control_p1[] PROGMEM = R"P3(
+<!-- ---- Led started here ---- -->
+<h2>Devices </h2>
+
+<div class="table-Control">
+    <div class="row">
+
+        <div class="fixedCell">
+         <div class="Status_bar bar2">)P3";
+
+
+////////////////for Led part 2///////////////
+const char Switch_one_control_p2[] PROGMEM = R"P4(</strong></div> 
+
+
+        </div><!--fixedCell -->
+
+              <div class="buttonsDiv">
+
+                <a class="ONoff" href="/stop?do=)P4";
+            
+ ////////////////for Led part 3///////////////           
+const char Switch_one_control[] PROGMEM = R"P5(            
+                </a>
+              </div> 
+
+    </div><!--row - ESP.com -->
+</div><!--table-Control -->)P5";
+
+/*LED1 Ends*/
+
+/*LED2 Starts*/
+const char Switch_two_control_p1[] PROGMEM = R"P6(
+
+<div class="table-Control">
+    <div class="row">
+
+        <div class="fixedCell">
+         <div class="Status_bar bar2">)P6";
+
+
+////////////////for Led part 2///////////////
+const char Switch_two_control_p2[] PROGMEM = R"P7(</strong></div> 
+
+
+        </div><!--fixedCell -->
+
+              <div class="buttonsDiv">
+
+                <a class="ONoff" href="/stop?do=)P7";
+            
+ ////////////////for Led part 3///////////////           
+const char Switch_two_control[] PROGMEM = R"P8(            
+                </a>
+              </div> 
+
+    </div><!--row - ESP.com -->
+</div><!--table-Control -->
+
+
+
+
+
+
+)P8";
+
+
+/*LED2 ENDS*/
+////End leds
